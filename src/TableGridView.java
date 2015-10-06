326244565c726817a64dff1a67061b04bcf2cb6f
blackstar <blackstar@blackstar-pc.rev.sfr.net>
2015-10-06 13:28:23 +0200

    Modif de base

---
+++ src/TableGridView.java
@@ -0,0 +1,13 @@
+import java.swing.*
+import java.awt.*;
+
+public class TableGridView extends JPanel{
+  
+  public void paintComponent(Graphics g){
+    for(int i=0; i<2; i++)
+      for(int i=0; i<10; i++){
+            
+      }
+    }
+  }
+}
